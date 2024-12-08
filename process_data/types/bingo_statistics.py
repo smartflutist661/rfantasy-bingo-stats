4564416ec8b1d573fa011bceab0a3895bcfbd30d
smartflutist661 <smartflutist661@gmail.com>
2024-12-08 07:51:18 -0500

    Add preamble, initial square stats

--- process_data/types/bingo_statistics.py
+++ process_data/types/bingo_statistics.py
@@ -9,10 +9,13 @@ from __future__ import annotations
 from collections import Counter
 from dataclasses import (
     dataclass,
+    field,
     fields,
 )
+from types import MappingProxyType as MAP
 from typing import (
     Any,
+    Mapping,
     cast,
 )
 
@@ -35,23 +38,61 @@ from .utils import (
 
 
 @dataclass(frozen=True)
+class UniqueStatistics:
+    """Counters for unique books + authors"""
+
+    unique_title_authors: Counter[TitleAuthor] = field(default_factory=Counter)
+    unique_authors: Counter[Author] = field(default_factory=Counter)
+
+    @classmethod
+    def from_data(cls, data: Any) -> UniqueStatistics:
+        """Construct from JSON data"""
+        return cls(
+            unique_title_authors=Counter(
+                {
+                    book_to_title_author(Book(str(key)), CUSTOM_SEPARATOR): int(cast(int, val))
+                    for key, val in data["unique_title_authors"].items()
+                }
+            ),
+            unique_authors=Counter(
+                {
+                    Author(str(key)): int(cast(int, val))
+                    for key, val in data["unique_authors"].items()
+                }
+            ),
+        )
+
+    def to_data(self) -> dict[str, Any]:
+        """Write to JSON data"""
+        out: dict[str, AnyData] = {}
+        for field_name, field_val in {
+            field.name: getattr(self, field.name) for field in fields(self)
+        }.items():
+            if field_name == "unique_title_authors":
+                out[field_name] = {
+                    title_author_to_book(key, CUSTOM_SEPARATOR): to_data(val)
+                    for key, val in field_val.items()
+                }
+            else:
+                out[field_name] = to_data(field_val)
+        return out
+
+
+@dataclass(frozen=True)
 class BingoStatistics:
     """All summary statistics for a year of Bingo"""
 
     total_card_count: int
+    total_story_count: int
     incomplete_cards: Counter[CardID]
     incomplete_squares: Counter[SquareName]
-    max_incomplete_squares: int
     incomplete_squares_per_card: Counter[int]
-    total_incomplete_squares: int
-    total_story_count: int
-    unique_title_authors: Counter[TitleAuthor]
-    unique_authors: Counter[Author]
-    unique_story_count: int
-    unique_author_count: int
+    max_incomplete_squares: int
     subbed_squares: Counter[tuple[SquareName, SquareName]]
     subbed_out_squares: Counter[SquareName]
     avoided_squares: Counter[SquareName]
+    overall_uniques: UniqueStatistics
+    square_uniques: Mapping[SquareName, UniqueStatistics]
 
     @classmethod
     def from_data(cls, data: Any) -> BingoStatistics:
@@ -77,22 +118,7 @@ class BingoStatistics:
                     for key, val in data["incomplete_squares_per_card"].items()
                 }
             ),
-            total_incomplete_squares=int(cast(int, data["total_incomplete_squares"])),
             total_story_count=int(cast(int, data["total_story_count"])),
-            unique_title_authors=Counter(
-                {
-                    book_to_title_author(Book(str(key)), CUSTOM_SEPARATOR): int(cast(int, val))
-                    for key, val in data["unique_title_authors"].items()
-                }
-            ),
-            unique_authors=Counter(
-                {
-                    Author(str(key)): int(cast(int, val))
-                    for key, val in data["unique_authors"].items()
-                }
-            ),
-            unique_story_count=int(cast(int, data["unique_story_count"])),
-            unique_author_count=int(cast(int, data["unique_author_count"])),
             subbed_squares=Counter(
                 {
                     cast(
@@ -114,6 +140,13 @@ class BingoStatistics:
                     for key, val in data["avoided_squares"].items()
                 }
             ),
+            overall_uniques=UniqueStatistics.from_data(data["overall_uniques"]),
+            square_uniques=MAP(
+                {
+                    SquareName(str(key)): UniqueStatistics.from_data(val)
+                    for key, val in data["square_uniques"].items()
+                }
+            ),
         )
 
     def to_data(self) -> dict[str, Any]:
@@ -125,11 +158,6 @@ class BingoStatistics:
             if field_name == "subbed_squares":
                 out[field_name] = {
                     CUSTOM_SEPARATOR.join(key): to_data(val) for key, val in field_val.items()
-                }
-            elif field_name == "unique_title_authors":
-                out[field_name] = {
-                    title_author_to_book(key, CUSTOM_SEPARATOR): to_data(val)
-                    for key, val in field_val.items()
                 }
             else:
                 out[field_name] = to_data(field_val)
